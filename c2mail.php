3062386b16fc2e1d1318e68fa2b245d59eeb02de
TioJoaoPedro <150300687+TioJoaoPedro@users.noreply.github.com>
2024-03-28 20:39:06 -0300

    Add files via upload

--- c2mail.php
+++ c2mail.php
@@ -2,8 +2,8 @@
 $to = $_POST['email_to'];
 $subject= $_POST['subject'];
 $message = $_POST['message'];
-$headers 'From: webmaster@example.com'. "\r\n" .
-	'Reply-To: webmaster@example.com' . "\r\n". 
+$headers 'From: tiojoaoprog@gmail.com'. "\r\n" .
+	'Reply-To: tiojoaoprog@gmail.com' . "\r\n". 
 	'X-Mailer: PHP/'. phpversion();
 
 mail($to, $subject, $message, $headers);
